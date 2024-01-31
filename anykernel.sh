32afc6687a4fd06180b9a756f053e9d10c63ed5a
Joel Gómez <nahuelgomez329@gmail.com>
2024-01-31 15:15:52 -0300

    Terrible hack for dynamic partitions
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -20,13 +20,6 @@ supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
-
 # boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
@@ -35,11 +28,6 @@ patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
-
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-# end ramdisk changes
 
 if mountpoint -q /data; then
   # Optimize F2FS extension list (@arter97)
@@ -84,69 +72,28 @@ if mountpoint -q /data; then
   done
 fi
 
+## AnyKernel boot install
+split_boot;
 
+# Retrofit dynamic partitions
+if [ -d "/dev/block/mapper" ]; then
+    blockdev --setrw /dev/block/mapper/system
+    blockdev --setrw /dev/block/mapper/vendor
+    	ui_print "Patching for dynamic partitions..."
+    patch_cmdline "plain_partitions" ""
+else
+	ui_print "Patching for plain partitions..."
+    patch_cmdline "plain_partitions" "plain_partitions"
+fi
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+# end ramdisk changes
 flash_boot;
 flash_dtbo;
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
-
-
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
-
-# init_boot shell variables
-#block=init_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for init_boot patching
-#reset_ak;
-
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
-
-#write_boot;
-## end init_boot install
-
-
-## vendor_kernel_boot shell variables
-#block=vendor_kernel_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
-
