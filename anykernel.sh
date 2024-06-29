24e1b2a4282da4db310eca1ecb9b9df0d32556e6
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2024-06-29 21:48:15 +0700

    Ak3: add banner
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Ryzen Kernel by Edwiin Kusuma Jaya @ telegram
+kernel.string=Ryzen Kernel [Retrofit Dynamic] by Ryuzenn @ telegram
 do.devicecheck=1
 do.modules=1
 do.systemless=0
